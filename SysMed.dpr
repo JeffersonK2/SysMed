2345797d990a67c88ec89d4765785f8caff15ef7
jefejc@hotmail.com <Jeffersonk2>
2019-10-14 20:55:13 -0300

    ttt

--- SysMed.dpr
+++ SysMed.dpr
@@ -4,9 +4,7 @@ uses
   Vcl.Forms,
   uMed in 'uMed.pas' {ulogin},
   UFrmenu in 'UFrmenu.pas' {Frmenu},
-  UFragendamento in 'UFragendamento.pas' {Frmagendamento},
-  uTeste in 'uTeste.pas' {Form1},
-  Unit2 in 'Unit2.pas' {Form2};
+  UFragendamento in 'UFragendamento.pas' {Frmagendamento};
 
 {$R *.res}
 
@@ -14,7 +12,5 @@ begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(Tulogin, ulogin);
-  Application.CreateForm(TForm1, Form1);
-  Application.CreateForm(TForm2, Form2);
   Application.Run;
 end.
